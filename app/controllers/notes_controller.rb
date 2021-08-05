df18ec1e4d63535a852fbb73918b68f9687901f2
Amanda <amandaben21@aol.com>
2021-08-04 22:43:56 -0400

    has a index, show, create & destroy

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,34 @@
-class NotesController < ApplicationController
+class Api::V1::NotesController < ApplicationController
+
+    def index
+        @notes = Note.all
+        render json: @notes
+    end
+    
+    def create
+        @note = Note.new(note_params)
+        if @note.save
+            render json: @note
+        else
+            render json: {error: 'Error creating note'}
+        end
+    end
+    
+    def show
+        @note = Note.find(params[:id])
+        render json: @note
+    end
+    
+    def destroy
+        @note = Note.find(params[:id])
+        @note.destroy
+    end
+    
+  
+    private
+
+    def note_params
+        params.require(:note).permit(:subject, :lesson_name, :date, :note_entry)
+    end
+
 end

a190235610c8213cdbc422acbb7be667331d54aa
Amanda <amandaben21@aol.com>
2021-08-04 23:00:38 -0400

    allowing the frontend to fetch here

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :reminders
-  resources :notes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+  namespace :api do
+    namespace :v1
+    resources :notes do
+      resources :reminders
+    end
+  end
 end

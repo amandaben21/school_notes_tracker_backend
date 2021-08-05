aa6a2ad850d0942125299cfef2fbd47a4f0b08cf
Amanda <amandaben21@aol.com>
2021-08-04 22:17:37 -0400

    has a index, show, create & destroy

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,2 +1,31 @@
-class RemindersController < ApplicationController
+class Api::V1::RemindersController < ApplicationController
+
+    def index
+      @reminders = Reminder.all
+      render json: @reminders
+    end
+
+    def create
+      @reminder = Reminder.new(reminder_params)
+      if @reminder.save
+        render json: @reminder
+      else
+        render json: {error: 'Error creating reminder'}
+      end
+    end
+
+    def show
+      @reminder = Reminder.find(params[:id])
+      render json: @reminder
+    end
+
+    def destroy
+      @reminder = Reminder.find(params[:id])
+      @reminder.destroy
+    end
+
+    private
+    def reminder_params
+        params.require(:reminder).permit(:content, :note_id)
+    end
 end
